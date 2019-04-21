137a5345ac969bf391f86e5b6506785a8f8d96cc
Ted Dang <dtnghia1987@gmail.com>
2019-04-21 13:38:13 -0600

    Update Ted's Scripts.sql

--- Ted's Scripts.sql
+++ Ted's Scripts.sql
@@ -1,1 +1,5 @@
-SELECT * FROM `t-notes`.user;
+CREATE DEFINER=`root`@`localhost` TRIGGER `user_AFTER_DELETE` AFTER DELETE ON `user` FOR EACH ROW BEGIN
+IF NEW.user_id > OLD.user_id THEN
+	SET NEW.user_id = NEW.user_id - 1;
+END IF;    
+END
