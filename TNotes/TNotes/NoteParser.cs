c1a0ab6a2be7e316e43bdfe884c79890b1f0728d
Draemon <bryce.harrington15@gmail.com>
2019-04-26 12:39:48 -0500

    making comments great again

--- TNotes/TNotes/NoteParser.cs
+++ TNotes/TNotes/NoteParser.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Linq;
 
 namespace TNotes
-{
+{   //This outlines friendly names for filetype management internally.
     enum FileType
     {
         TXT,
@@ -20,9 +20,10 @@ namespace TNotes
         {
 
         }
+        //Take a filename, identify the extension, and parse it appropriately.
         public List<string> parseFile(string filename)
         {
-            StreamReader input = new StreamReader(filename);             
+            StreamReader input = new StreamReader(filename);
             string[] s = filename.Split('.');
             FileType t;
             if (s[1].Equals("txt"))
@@ -40,33 +41,42 @@ namespace TNotes
             }
             return parseFileType(input, t);
         }
+        // Split on the appropriate fileType, and go. 
         public List<string> parseFileType(StreamReader input, FileType t)
         {
+            // parse the appropriate filetype. 
             switch (t)
             {
-                case FileType.TXT : return parseTXTFile(input);
-                case FileType.RTF : return parseRTFFile(input);
+                case FileType.TXT: return parseTXTFile(input);
+                case FileType.RTF: return parseRTFFile(input);
                 default: return new List<string>();
             }
         }
+        // parseTXTFile takes a txt file StreamReader, and generates a list of Strings 
+        // to be passed into the includeMultiple() function from SQLInterface
         private List<string> parseTXTFile(StreamReader input)
         {
             List<string> tokens = new List<string>();
             string line;
+            //The delimiters specify all of the characters which have no semantic value in a note
+            //for splitting information.
             char[] delims = { ' ', '\t', '\r', ',', '.', '!', '?', '`', '~' , '|'
                             , '(', ')' , '{' , '}', '[', ']', '<', '>', '/' , '\\'
                             , '@', '$' , '%' , '^', '&', '*', '-', '_', '+' , '='
                             , '1', '2' , '3' , '4', '5', '6', '7', '8', '9' , '0'
                             , ':', ';' , '\'', '\"'
                             };
-            while((line = input.ReadLine()) != null)
+            while ((line = input.ReadLine()) != null)
             {
+                //Split each line of the file over the delimiters:
                 string[] s = line.Split(delims);
-                for(int i = 0; i < s.Length; i++)
+                //Add each string to the token list. 
+                for (int i = 0; i < s.Length; i++)
                 {
                     tokens.Add(s[i].Trim());
                 }
             }
+            //Filter out the garbage, and smile. 
             return removeGarbage(tokens);
         }
         private List<string> parseRTFFile(StreamReader input)
@@ -103,13 +113,18 @@ namespace TNotes
 
             // Output plain text to file, encoded as UTF-8.
             System.IO.File.WriteAllText("temp.txt", plainText);
-            
-            return parseTXTFile(new StreamReader("temp.txt")); 
+
+            return parseTXTFile(new StreamReader("temp.txt"));
         }
+        //This should remove meaningless words to prevent filler words from flooding
+        //the keyword database.
         private List<string> removeGarbage(List<string> tokens)
         {
+            //Create the filter for the words that aren't needed from the string list.
             tokens = removeEmpty(tokens);
-            string[] aa   = {"a","an","the"};
+            //Article Adjectives:
+            string[] aa = { "a", "an", "the" };
+            //Prepositions:
             string[] prep = { "of", "with", "at", "from", "into", "during"
                             , "including", "until", "against", "among", "throughout"
                             , "despite", "towards", "upon", "concerning", "to", "in"
@@ -118,14 +133,16 @@ namespace TNotes
                             , "within", "along", "following", "across", "behind", "beyond"
                             , "plus", "except", "but", "up", "out", "around", "down"
                             , "off", "above", "near" };
-            string[] cv   = { "am", "is", "are", "was", "were", "be", "being", "been" };
-            string[] pn   = { "it", "i", "you", "he", "they", "we", "she", "who", "them"
+            //Common Verb:
+            string[] cv = { "am", "is", "are", "was", "were", "be", "being", "been" };
+            //Pronouns: 
+            string[] pn = { "it", "i", "you", "he", "they", "we", "she", "who", "them"
                             , "me", "him", "one", "her", "us" };
-
-            for(int i = tokens.Count; i > -1; i--)
+            //Loop through the list, and remove any copies of the filtered words.
+            for (int i = tokens.Count; i > -1; i--)
             {
                 string s = tokens.ElementAt(i).ToLower();
-                if ( aa.Contains(s)
+                if (aa.Contains(s)
                   || prep.Contains(s)
                   || cv.Contains(s)
                   || pn.Contains(s)
@@ -136,12 +153,13 @@ namespace TNotes
             }
             return tokens;
         }
+        //Pass through the list and remove any null or empty strings. 
         private List<string> removeEmpty(List<string> tokens)
         {
-            for(int i = tokens.Count; i > -1; i--)
+            for (int i = tokens.Count; i > -1; i--)
             {
                 string s = tokens.ElementAt(i);
-                if ( s.Equals("")
+                if (s.Equals("")
                   || s.Equals(null))
                 {
                     tokens.RemoveAt(i);
